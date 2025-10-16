130b1af80a44e492e8ee4f9ec0f6004395094b8f
mmelero <mmelero@gmail.com>
2025-10-16 20:21:23 -0300

    first commit

---
+++ src/main/java/com/deliverytech/deleivery_api/DeleiveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.deliverytech.deleivery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleiveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleiveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliverytech/deleivery_api/DeleiveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverytech.deleivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleiveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
